6872dcb98d7f338f6f8c6805578fd0eebbf21803
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2023-03-09 18:41:35 -0300

    Finalizando recebimento das variaveis e manipulacao no editform , alteracao da classe remedios com novos atributos

--- Portal-Remedios/Portal-Remedios/Models/Regiao.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Portal_Remedios.Models
-{
-    public class Regiao
-    {
-        public string Nome { get; set; }
-        public int Id { get; set; }
-    }
-}
--- Portal-Remedios/Portal-Remedios/Models/Remedios.cs
+++ Portal-Remedios/Portal-Remedios/Models/Remedios.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Text;
 
-namespace Remedios
+namespace remedios
 {
 
 
@@ -13,18 +13,35 @@ namespace Remedios
         Preta
 
     }
+    public enum Tipo
+    {
+        Fitoterápico,
+        Homeopático,
+        Similar,
+        Genérico,
+        Referência,
+        Manipulado,
+        Outros
+    }
 
 
 
     public class Remedios
     {
         public int Id { get; set; }
-        public string? Nome { get; set; }
+        public string Nome { get; set; }
+        public int Codigo { get; set; }
+        public string Codigo_ANS { get; set; }
+        public DateTime Data_lote { get; set; }
+        public string Vaga_lote { get; set; }
+        public DateTime Data_deposito { get; set; }
+        public Tipo Tipo_remedio { get; set; }
         public Classificacao Cor { get; set; }
-        public string? Descricao { get; set; }
         public DateTime Hora_Cadastro { get; set; }
+        public string Img_Remedio { get; set; }
         public string? Link_Bula { get; set; }
-
+        public string Nome_Regiao { get; set; }
+        public string Nome_Unidade { get; set; }
 
     }
 
