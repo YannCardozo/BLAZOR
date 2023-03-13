6b23d2b087ac88db3221a98135daec82007040d6
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2023-03-13 16:58:47 -0300

    Adicionado API via CEP FACIL , busca cep facilita

--- Portal-Remedios/Portal-Remedios/Models/Cep.cs
+++ Portal-Remedios/Portal-Remedios/Models/Cep.cs
@@ -3,6 +3,7 @@
     public class Cep
     {
         public string cep { get; set; }
+        public string cidade { get; set; }
         public string logradouro { get; set; }
         public string complemento { get; set; }
         public string bairro { get; set; }
