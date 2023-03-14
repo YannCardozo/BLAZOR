8e3faed72d92523a14a9c37fb2fcac237b3e4122
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2023-03-14 17:40:29 -0300

    Finalizacao Modal cadastro de beneficiario e inclusao da model Beneficiario

---
+++ Portal-Remedios/Portal-Remedios/Models/Beneficiario.cs
@@ -0,0 +1,18 @@
+﻿namespace Portal_Remedios.Models
+{
+    public class Beneficiario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public DateTime data_nasc { get; set; }
+        public string sexo { get; set; }
+        public string ocupacao { get; set; }
+        public string NIS { get; set; }
+        public string escolaridade { get; set; }
+        public string motivo_cadastro { get; set; }
+        public string renda_familiar { get; set; }
+
+
+    }
+}
