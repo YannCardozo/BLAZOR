af884fb2e8a08c3e71d893f192880dca92cde455
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2023-04-14 08:32:18 -0300

    Testando deploy vercel app

---
+++ Portal-Remedios/Models/Usuarios.cs
@@ -0,0 +1,13 @@
+﻿namespace Portal_Remedios.Models
+{
+    public class Usuarios
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public string role { get; set; }
+
+        
+    }
+}
