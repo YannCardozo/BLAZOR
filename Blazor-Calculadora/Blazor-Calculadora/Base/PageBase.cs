38035935f0c7d2622a275032b2bb467e039d302b
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2023-07-24 00:19:45 -0300

    Alteracao responsividade dispositivos, correc de bug correcao do bug de abertura do DOM ao clicar no AC , imposto breakpoints para responsividade de dispositivos

---
+++ Blazor-Calculadora/Blazor-Calculadora/Base/PageBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Blazor_Calculadora.Base.PageBase;
+
+public class PageBase : ComponentBase
+{
+    [Inject] public IJSRuntime JSRuntime { get; set; }
+    [Inject] public NavigationManager NavigationManager { get; set; }
+}
---
+++ Blazor-Calculadora/Blazor-Calculadora/Pages/Calculadora.razor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Blazor_Calculadora.Base.PageBase;
+
+namespace Blazor_Calculadora.Pages
+{
+    public class CalculadoraBack : PageBase
+    {
+        public string input = "";
+        public string result = null;
+        public bool mostralista = false;
+        public List<string> historicoContas = new List<string>();
+
+
+        public void AppendNumber(int number)
+        {
+
+            input += number.ToString();
+        }
+
+        public void AppendOperator(char op)
+        {
+            input += " " + op + " ";
+        }
+
+        public void AppendDecimal()
+        {
+            if (!input.Contains("."))
+                input += ".";
+        }
+
+        public void Clear()
+        {
+            input = "";
+            result = "";
+            //mostralista = true;
+        }
+
+        public void Calculate()
+        {
+            try
+            {
+                result = new DataTable().Compute(input, null).ToString();
+                string resultado = new DataTable().Compute(input, null).ToString();
+                if (resultado != "")
+                {
+                    historicoContas.Insert(0, $"{input} = {resultado}");
+                }
+                if (historicoContas.Count > 10)
+                {
+                    historicoContas.RemoveAt(historicoContas.Count - 1);
+                }
+                input = result;
+                if (input != "")
+                {
+                    mostralista = true;
+                }
+
+            }
+            catch (Exception)
+            {
+                result = "Error"; // Handle invalid input or calculation error
+            }
+        }
+        public void DeletaLista()
+        {
+            result = "";
+            historicoContas.Clear();
+            mostralista = false;
+        }
+    }
+}
