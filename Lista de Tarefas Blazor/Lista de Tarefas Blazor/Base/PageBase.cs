5eb131ec2eda4415aa4037809f583ecfb2cc103f
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2023-07-25 08:55:02 -0300

    Inicio projeto lista de tarefas no blazor

---
+++ Lista de Tarefas Blazor/Lista de Tarefas Blazor/Base/PageBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Lista_de_Tarefas_Blazor.Base
+{
+    public class PageBase : ComponentBase
+    {
+        [Inject] public IJSRuntime JSRuntime { get; set; }
+        [Inject] public NavigationManager NavigationManager { get; set; }
+    }
+}
---
+++ Lista de Tarefas Blazor/Lista de Tarefas Blazor/Pages/Lista.razor.cs
@@ -0,0 +1,8 @@
+﻿using Lista_de_Tarefas_Blazor.Base;
+
+namespace Lista_de_Tarefas_Blazor.Pages
+{
+    public class ListaBase : PageBase
+    {
+    }
+}
