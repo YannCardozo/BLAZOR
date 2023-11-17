e1102ac8ce0e1ad79666539f228f915a8820cac1
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2023-11-17 01:02:37 -0300

    Inicio do sistema JusProcessos, implementado conexao do banco de dados com o identity, funcionando ate aqui

---
+++ JusProcessos/JusProcessos/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using JusProcessos.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JusProcessos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+
+
+        }
+        public DbSet<Thor> Teste { get; set; }
+    }
+}
