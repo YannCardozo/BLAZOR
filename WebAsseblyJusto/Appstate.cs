2fd4650d51d9ac0bd77250584515102609999b91
Yann Salgado <82240581+YannCardozo@users.noreply.github.com>
2024-05-08 17:00:38 -0300

    Subindo webassembly justo para testar com projeto webapi justovaldir

---
+++ WebAsseblyJusto/Appstate.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorApp2
+{
+    public static class Appstate
+    {
+        public static string LoggedUser { get; set; } = string.Empty;
+    }
+}
---
+++ WebAsseblyJusto/Pages/models/Advogado.cs
@@ -0,0 +1,20 @@
+﻿namespace WebAsseblyJusto.Pages.models
+{
+    public class Advogado
+    {
+        public Advogado()
+        {
+
+        }
+        public Advogado(string cPF, string oAB, int id)
+        {
+            CPF = cPF;
+            OAB = oAB;
+            this.id = id;
+        }
+
+        public string CPF { get; set; }
+        public string OAB { get; set; }
+        public int id { get; set; }
+    }
+}
